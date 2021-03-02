72d98aeeb93e86e77f9c883fcb2968550d1539cf
Marvin Heilemann <m.heilemann@protonmail.com>
2021-03-02 11:55:42 +0100

    chore: bump version

--- pkg.ts
+++ pkg.ts
@@ -7,8 +7,8 @@ interface Pkg {
 export const getPkg = (): Pkg => {
   return {
     name: "webssl",
+    version: "3.5.4",
     description:
       "Simply create an OpenSSL certificate for your local web or mobile development that just work!",
-    version: "3.5.3",
   };
 };
